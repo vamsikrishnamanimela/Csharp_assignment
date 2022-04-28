cd43b15784ff8e816da057e6ee5a9fea3526334f
vamsikrishnamanimela <103919216+vamsikrishnamanimela@users.noreply.github.com>
2022-04-28 20:23:57 +0530

    Update Program.cs

--- c#/exam1/exam1/Program.cs
+++ c#/exam1/exam1/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace exam1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            bool x = true;
-            Console.WriteLine(Convert.ToString(x));
-        }
-    }
-}
